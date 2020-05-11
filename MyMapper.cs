350de2d51f54da68f815473199c0c781c0f225d6
Serhiy <yaroshevich.serh@gmail.com>
2020-05-11 13:00:00 +0300

    Add project files.

---
+++ MyMapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mapper
+{
+    public static class MyMapper<TDestination>
+    {
+        public static TDestination Map(object source) 
+        {
+
+        } 
+    }
+}
+
